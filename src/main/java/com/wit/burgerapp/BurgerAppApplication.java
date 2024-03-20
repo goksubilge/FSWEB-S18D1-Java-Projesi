ae8e99b7467ff9de0656aa3107640ee37e20c59c
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-03-20 18:11:55 +0300

    init project

---
+++ src/main/java/com/wit/burgerapp/BurgerAppApplication.java
@@ -0,0 +1,13 @@
+package com.wit.burgerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wit/burgerapp/BurgerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wit.burgerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
