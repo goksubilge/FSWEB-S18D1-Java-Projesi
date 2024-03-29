eb47f7bf1fe3d21a716fd5a317288ea83a3819f4
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-03-29 19:24:21 +0300

    edit mission 3 (findById, findByBreadType, findByContent)

--- src/main/java/com/wit/burgerapp/controller/BurgerController.java
+++ src/main/java/com/wit/burgerapp/controller/BurgerController.java
@@ -40,24 +40,24 @@ public class BurgerController {
     // id yoksa -> save ,  id varsa -> update yapacak tek bir method yazacağız yarın. "POST + PUT" birleşecek artık.
 
     @DeleteMapping("/{id}")
-    public Burger remove(int id){
+    public Burger remove(@PathVariable  int id){
         return burgerDao.remove(id);
     }
 
     //findByPrice
     @GetMapping("/findByPrice/{price}")
-    public List<Burger> findById(double price){
+    public List<Burger> findByPrice(@PathVariable double price){
         return burgerDao.findByPrice(price);
     }
     //findByBreadType
     @GetMapping("/findByBreadType/{findByBreadType}")
-    public List<Burger> findByBreadType(@PathVariable String breadType) {
-        BreadType breadTypeEnum = BreadType.valueOf(breadType);
+    public List<Burger> findByBreadType(@PathVariable String findByBreadType) {
+        BreadType breadTypeEnum = BreadType.valueOf(findByBreadType);
         return burgerDao.findByBreadType(breadTypeEnum);
     }
     //findByContent
     @GetMapping("/findByContent/{findByContent}")
-    public List<Burger> findByContent(@PathVariable String content ) {
-        return burgerDao.findByContent(content);
+    public List<Burger> findByContent(@PathVariable String findByContent ) {
+        return burgerDao.findByContent(findByContent);
     }
 }
