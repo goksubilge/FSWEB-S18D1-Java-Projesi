7e708335881484d2f4727b661e2e5dcac9a8b85c
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-03-29 18:20:31 +0300

    add mission 3 (findById, findByBreadType, findByContent)

--- src/main/java/com/wit/burgerapp/controller/BurgerController.java
+++ src/main/java/com/wit/burgerapp/controller/BurgerController.java
@@ -1,6 +1,7 @@
 package com.wit.burgerapp.controller;
 
 import com.wit.burgerapp.dao.BurgerDao;
+import com.wit.burgerapp.entity.BreadType;
 import com.wit.burgerapp.entity.Burger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -43,4 +44,20 @@ public class BurgerController {
         return burgerDao.remove(id);
     }
 
+    //findByPrice
+    @GetMapping("/findByPrice/{price}")
+    public List<Burger> findById(double price){
+        return burgerDao.findByPrice(price);
+    }
+    //findByBreadType
+    @GetMapping("/findByBreadType/{findByBreadType}")
+    public List<Burger> findByBreadType(@PathVariable String breadType) {
+        BreadType breadTypeEnum = BreadType.valueOf(breadType);
+        return burgerDao.findByBreadType(breadTypeEnum);
+    }
+    //findByContent
+    @GetMapping("/findByContent/{findByContent}")
+    public List<Burger> findByContent(@PathVariable String content ) {
+        return burgerDao.findByContent(content);
+    }
 }
